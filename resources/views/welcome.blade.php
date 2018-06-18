8a2b8139dd43fd0633f13c00195e3d9c0cda4974
ruofeng <lrf0829@126.com>
2018-06-18 14:55:06 +0800

    update course arrange

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>教学管理</title>
-    <link as=style href=/css/app.3302c3b5.css rel=preload>
+    <link as=style href=/css/app.985efd84.css rel=preload>
     <link as=style href=/css/chunk-vendors.a6fa1a74.css rel=preload>
-    <link as=script href=/js/app.7ab61caf.js rel=preload>
+    <link as=script href=/js/app.199ac7bf.js rel=preload>
     <link as=script href=/js/chunk-vendors.a51aa4ee.js rel=preload>
     <link href=/css/chunk-vendors.a6fa1a74.css rel=stylesheet>
-    <link href=/css/app.3302c3b5.css rel=stylesheet>
+    <link href=/css/app.985efd84.css rel=stylesheet>
 </head>
 
 <body>
@@ -20,7 +20,7 @@
     </noscript>
     <div id=app></div>
     <script src=/js/chunk-vendors.a51aa4ee.js></script>
-    <script src=/js/app.7ab61caf.js></script>
+    <script src=/js/app.199ac7bf.js></script>
 </body>
 
 </html>
