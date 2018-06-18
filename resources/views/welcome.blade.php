88ad29753196f8f41827182729fb797b695631fd
ruofeng <lrf0829@126.com>
2018-06-18 22:11:21 +0800

    fix register role

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <title>教学管理</title>
     <link as=style href=/css/app.43872f0d.css rel=preload>
     <link as=style href=/css/chunk-vendors.a6fa1a74.css rel=preload>
-    <link as=script href=/js/app.063eddfa.js rel=preload>
+    <link as=script href=/js/app.3eb95027.js rel=preload>
     <link as=script href=/js/chunk-vendors.a51aa4ee.js rel=preload>
     <link href=/css/chunk-vendors.a6fa1a74.css rel=stylesheet>
     <link href=/css/app.43872f0d.css rel=stylesheet>
@@ -20,7 +20,7 @@
     </noscript>
     <div id=app></div>
     <script src=/js/chunk-vendors.a51aa4ee.js></script>
-    <script src=/js/app.063eddfa.js></script>
+    <script src=/js/app.3eb95027.js></script>
 </body>
 
 </html>
