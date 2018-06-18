3b84c065c06c69960c67bde1e2a20b168825ce0a
ruofeng <lrf0829@126.com>
2018-06-18 22:07:09 +0800

    some fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,12 @@
     <meta http-equiv=X-UA-Compatible content="IE=edge">
     <meta name=viewport content="width=device-width,initial-scale=1">
     <title>教学管理</title>
-    <link as=style href=/css/app.615d60e0.css rel=preload>
+    <link as=style href=/css/app.43872f0d.css rel=preload>
     <link as=style href=/css/chunk-vendors.a6fa1a74.css rel=preload>
-    <link as=script href=/js/app.efaa68ea.js rel=preload>
+    <link as=script href=/js/app.063eddfa.js rel=preload>
     <link as=script href=/js/chunk-vendors.a51aa4ee.js rel=preload>
     <link href=/css/chunk-vendors.a6fa1a74.css rel=stylesheet>
-    <link href=/css/app.615d60e0.css rel=stylesheet>
+    <link href=/css/app.43872f0d.css rel=stylesheet>
 </head>
 
 <body>
@@ -20,7 +20,7 @@
     </noscript>
     <div id=app></div>
     <script src=/js/chunk-vendors.a51aa4ee.js></script>
-    <script src=/js/app.efaa68ea.js></script>
+    <script src=/js/app.063eddfa.js></script>
 </body>
 
 </html>
